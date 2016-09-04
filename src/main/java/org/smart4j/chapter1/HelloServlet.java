833fa178ff8de32a9ff0c18de7900537f388a42b
java_club <zhaopan523@126.com>
2016-09-04 17:15:37 +0800

    commit Message

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package org.smart4j.chapter1;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,
+                resp);
+    }
+
+}
